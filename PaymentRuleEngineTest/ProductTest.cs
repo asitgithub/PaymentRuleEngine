69ded428df260e53c5e4a11d819cd65fa4965ac6
Asit Singh <Asit_S@dellteam.com>
2020-09-01 11:54:50 +0530

    Test for 1 & 2

--- PaymentRuleEngineTest/ProductTest.cs
+++ PaymentRuleEngineTest/ProductTest.cs
@@ -3,12 +3,30 @@ using Xunit;
 
 namespace PaymentRuleEngineTest
 {
-	public class ProductTest
-	{
-		[Fact]
-		public void Test1()
-		{
+    public class PhysicalProductTests : IClassFixture<PhysicalProductFixture>
+    {
+        private PhysicalProductFixture _fixture;
 
-		}
-	}
+        public PhysicalProductTests(PhysicalProductFixture fixture)
+        {
+            _fixture = fixture;
+
+            _fixture.mockSlipGenerator = new Mock<IPackingSlipGenerator>();
+
+            _fixture.physicalProduct = new PhysicalProduct(_fixture.mockSlipGenerator.Object);
+        }
+
+        [Fact]
+        public void GeneratePackingSlipForShippingMustGenerateSlip()
+        {
+            _fixture.physicalProduct.GeneratePackingSlipForShipping();
+            return;
+        }
+    }
+
+    public class PhysicalProductFixture
+    {
+        //public PhysicalProduct physicalProduct;
+        //public Mock<IPackingSlipGenerator> mockSlipGenerator;
+    }
 }
