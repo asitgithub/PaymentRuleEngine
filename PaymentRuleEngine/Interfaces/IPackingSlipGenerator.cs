7732e872f55ae80539f5d7613f10d0b85e3de2f2
Asit Singh <Asit_S@dellteam.com>
2020-09-01 11:49:31 +0530

    Requirement 1 & 2 Implementation

---
+++ PaymentRuleEngine/Interfaces/IPackingSlipGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentRuleEngine.Interfaces
+{
+	public class IPackingSlipGenerator
+	{
+		void GeneratePackingSlip(PackingSlipDestination _packingSlipDestination);
+	}
+}
---
+++ PaymentRuleEngine/Resources/PackingSlipType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentRuleEngine.Resources
+{
+	public enum PackingSlipDestination
+	{
+		Shipping = 1,
+		RoyaltyDepartment = 2
+	}
+}
